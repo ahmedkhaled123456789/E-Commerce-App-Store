c9526fa75b1e74c8897ab8104f9ce2f28c8d672d
brso <beso2000elsheikh1999@gmail.com>
2024-12-09 22:26:16 +0200

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
- 
+
 const Home = () => {
   return (
     <div className="bg-blue-500 text-white text-center p-5">
-      <h1 className="text-2xl  ">Hello <i className="ri-heart-line"></i></h1>
-      
+
     </div>
   )
 }
